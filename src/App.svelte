b1a6aac35cc4f46c42cfce86c8b6bc59f15f51f6
Neos21 <neos21@gmail.com>
2020-07-31 09:16:21 +0900

    npm i -D sass And Write SCSS

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
@@ -20,6 +20,12 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	p {
+		a {
+			text-decoration: overline underline;
+		}
 	}
 
 	@media (min-width: 640px) {
